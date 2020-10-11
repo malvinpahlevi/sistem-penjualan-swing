952ac62b91a56463b6a24936e45d63e65ba96e01
malvinpahlevi <malvinfrog@gmail.com>
2020-10-11 21:33:22 +0700

    adding constructor Controller_Barang

---
+++ src/Controller/Controller_Barang.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+import DAO.DAO_Barang;
+import Model.Barang;
+import View.MBarang;
+import java.awt.Color;
+import java.util.List;
+
+/**
+ *
+ * @author FOLIO 1040 G3
+ */
+public class Controller_Barang {
+    MBarang form;
+    DAO_Barang model;
+    List<Barang> list; //deklarasi method "List" yang sudah dibuat pada interface MODEL_DAO
+    String[] header; //deklarasi variable untuk membuat judul kolom pada object JTabel
+    
+    //konstruktor
+    public Controller_Barang(MBarang form){
+        this.form = form;
+        model = new DAO_Barang();
+        list = model.getAll();
+        header = new String[]{"KODE BARANG", "NAMA BARANG", "SATUAN", "HARGA", "STOK","KODE KATEGORI", "NAMA KATEGORI"};
+        
+        form.getTblbarang().setShowGrid(true);
+        form.getTblbarang().setShowVerticalLines(true);
+        form.getTblbarang().setGridColor(Color.blue);    
+    }
+}
